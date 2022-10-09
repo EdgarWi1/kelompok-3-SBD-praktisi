063337835158075cf35a6349ea042a122e79f30d
Muhkahfims <114171924+Muhkahfims@users.noreply.github.com>
2022-10-10 07:50:20 +0800

    Add files via upload

---
+++ DCL.sql
@@ -0,0 +1,26 @@
+# Buat user baru
+CREATE USER 'Sisfo'@'localhost' IDENTIFIED BY '2021';
+
+# Beri hak akses ke user 
+GRANT CREATE, DROP, SELECT, INSERT, DELETE, UPDATE 
+ON contract_sisfo
+TO 'Sisfo'@'localhost';
+
+# Beri akses tertentu (misal akses SELECT) kepada semua user
+GRANT SELECT 
+ON contract_sisfo
+TO '*'@'localhost';
+
+# Cek status hak akses user
+SHOW GRANTS FOR 'Sisfo'@'localhost';
+
+# Hapus hak akses user
+REVOKE INSERT
+ON contract_sisfo
+FROM 'Sisfo'@'localhost';
+
+# Hapus akses tertentu (misal akses DROP) kepada semua user
+REVOKE DROP
+ON contract_sisfo
+FROM '*'@'localhost';
+
