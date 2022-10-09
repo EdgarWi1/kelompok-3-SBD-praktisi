65958bb39b6049b5e3b3a20c7a66af7d21c4a46c
Muhkahfims <114171924+Muhkahfims@users.noreply.github.com>
2022-10-10 07:56:34 +0800

    Update DCL.sql

--- DCL.sql
+++ DCL.sql
@@ -3,12 +3,12 @@ CREATE USER 'Sisfo'@'localhost' IDENTIFIED BY '2021';
 
 # Beri hak akses ke user 
 GRANT ALL PRIVILEGES 
-ON contract_sisfo
+ON db_praktisi
 TO 'Sisfo'@'localhost';
 
 # Beri akses tertentu (misal akses SELECT) kepada semua user
 GRANT SELECT 
-ON contract_sisfo
+ON db_praktisi
 TO '*'@'localhost';
 
 # Cek status hak akses user
@@ -16,11 +16,11 @@ SHOW GRANTS FOR 'Sisfo'@'localhost';
 
 # Hapus hak akses user
 REVOKE INSERT
-ON contract_sisfo
+ON db_praktisi
 FROM 'Sisfo'@'localhost';
 
 # Hapus akses tertentu (misal akses DROP) kepada semua user
 REVOKE DROP
-ON contract_sisfo
+ON db_praktisi
 FROM '*'@'localhost';
 
